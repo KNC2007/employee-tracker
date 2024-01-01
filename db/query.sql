fec950cd4baa7988d01863f9355272da116ae310
Courtney Kalbach <courtneykalbach@Courtneys-MacBook-Pro.local>
2024-01-01 13:54:57 -0500

    fixing try catch promise based syntax and table displays

--- db/query.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-FROM course_names /* COURSE */
-JOIN department 
-/*
-  FOREIGN KEY (department)
-  REFERENCES department(id)
-*/
-ON course_names.department = department.id;
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const { table } = require('table');
 const inquirer = require('inquirer');
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
+const startConnection = require('./db/connection')
+let db = null;
 
-// let db = null; should i have this??
+const config = {
+  columns: {
+    0: { alignment: 'left' },
+    1: { alignment: 'left' },
+  },
+  border: {
+    topBody: `─`,
+    topJoin: `┬`,
+    topLeft: `┌`,
+    topRight: `┐`,
+
+    bottomBody: `─`,
+    bottomJoin: `┴`,
+    bottomLeft: `└`,
+    bottomRight: `┘`,
+
+    bodyLeft: `│`,
+    bodyRight: `│`,
+    bodyJoin: `│`,
+
+    joinBody: `─`,
+    joinLeft: `├`,
+    joinRight: `┤`,
+    joinJoin: `┼`
+  }
+};
+
 
 const questions = [
   {
     type: 'list',
-    name: 'selectOption',
+    name: 'option',
     message: 'What would you like to do?',
     choices: [
       'View All Departments',
@@ -27,92 +55,122 @@ const questions = [
   },
 ]
 
-const init = async () => {
-  db = await mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'rootroot',
-      database: 'employeeTracker_db'
-    }
-  )
-  startMenu();
-};
 
-console.log(`Connected to the employeeTracker_db database.`);
-console.log(db);
+
+async function init() {
+  // connect to database
+  db = await startConnection();
+  // console.log(db);
+  // const results = await db.query('SELECT * FROM department');
+  // get data from results
+  // console.log(results[0]);
+  console.log(`Connected to the employeeTracker_db database.`);
+  // console.log(db);
+  await startMenu();
+}
+
 
 // GIVEN a command-line application that accepts user input
 // WHEN I start the application
 // THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 async function startMenu() {
   const answers = await inquirer.prompt(questions)
-  console.log(answers);
-  const choice = await answers.choice;
+  // console.log(answers);
+  const choice = await answers.option;
+  // console.log(choice);
   switch (choice) {
     case 'View All Departments':
-      return viewAllDepartments();
+      viewAllDepartments();
       break;
     case 'View All Roles':
-      return viewAllRoles();
+      viewAllRoles();
       break;
     case 'View All Employees':
-      return viewAllEmployees();
+      viewAllEmployees();
       break;
     case 'Add a Department':
-      return addDepartment();
+      addDepartment();
       break;
     case 'Add a Role':
-      return addRole();
+      addRole();
       break;
     case 'Add an Employee':
-      return addEmployee();
+      addEmployee();
       break;
     case 'Update Employee Role':
-      return captureInput();
+      captureInput();
       break;
     case 'Update Employee Manager':
-      return captureInput();
+      captureInput();
       break;
     case 'View Employees by Manager':
-      return captureInput();
+      captureInput();
       break;
     case 'View Employees by Department':
-      return captureInput();
+      captureInput();
       break;
     case 'Delete departments, roles, and employees':
-      return captureInput();
+      captureInput();
       break;
     case 'View the total utilized budget of a department':
-      return captureInput();
+      captureInput();
       break;
-    default: Quit()
+    case 'Quit':
+      Quit()
+      break;
   }
 };
 
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
-function viewAllDepartments() {
-  const query = db.query('SELECT * FROM department;', function (err, results) {
-    console.log(results);
-  })
+async function viewAllDepartments() {
+  try {
+    const results = await db.query("SELECT * FROM department");
+    // console.log(results[0]);
+    console.log("Viewing All Departments: ");
+    const displayTable = results[0].map(row => Object.values(row));
+    // console.table(displayTable);
+    displayTable.unshift(["id", "name"]);
+    // console.table(displayTable);
+    console.log(table(displayTable, config));
+  } catch (error) {
+    console.error(error);
+  }
+  init();
 };
+
 
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-function viewAllRoles() {
-  const query = db.query('SELECT * FROM role;', function (err, results) {
-    console.log(results);
-  })
+async function viewAllRoles() {
+  try {
+    const results = await db.query('SELECT * FROM role');
+    console.log("Viewing All Roles: ");
+    const displayTable = results[0].map(row => Object.values(row));
+    displayTable.unshift(["id", "title", "salary", "department_id"]);
+    console.log(table(displayTable, config));
+  } catch (error) {
+    console.error(error);
+  }
+  init();
 };
+
 
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-function viewAllEmployees() {
-  db.query('SELECT * FROM employee', function (err, results) {
-    console.log(results);
-  })
+async function viewAllEmployees() {
+  try {
+    const results = await db.query('SELECT * FROM employee');
+    console.log("Viewing All Employees: ");
+    const displayTable = results[0].map(row => Object.values(row));
+    displayTable.unshift(["id", "first_name", "last_name", "role_id", "manager_id"]);
+    console.log(table(displayTable, config));
+  } catch (error) {
+    console.error(error);
+  }
+  init();
 };
+
 
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
@@ -207,6 +265,43 @@ async function addEmployee() {
 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+async function addEmployeeRole() {
+  const results = await db.query("SELECT id, title FROM role");
+  console.log(results);
+  const roles = results.map(({ id, title }) => ({
+    name: title,
+    value: id,
+  }))
+  const newEmployee = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: "Enter the employee's first name.",
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: "Enter the employee's last name.",
+    },
+    {
+      type: 'list',
+      name: 'role_Id',
+      message: "Select the employee's role.",
+      choices: roles,
+    },
+    {
+      type: 'list',
+      name: 'manager_Id',
+      message: "Select the employee's manager.",
+      choices: [
+        { name: "None", value: null },
+        ...managers,
+      ],
+    }
+  ])
+  db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", answers.firstName, answers.lastName, answers.role_Id, answers.manager_Id,);
+  console.log('Employee added to database.')
+};
 
 
 
@@ -221,11 +316,10 @@ async function addEmployee() {
 
 
 
-
-function Quit() {
-  console.log("exitting")
-  process.exit()
-}
+// function Quit() {
+//   console.log("exitting")
+//   process.exit()
+// }
 
 
 // // table module
@@ -236,8 +330,10 @@ function Quit() {
 // console.log(table(arrOfArr));
 
 
-init();
 
+
+init();
+// startMenu();
 
 
 
